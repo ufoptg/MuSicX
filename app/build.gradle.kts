bba7af688b1afff02c6aaee206ed80e8ede38df6
TrueSaiyan <perthgroups@protonmail.com>
2026-07-02 22:47:25 +0800

    Bump version code and name to 152 and 13.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 151
-        versionName = "13.6.4"
+        versionCode = 152
+        versionName = "13.6.5"
         resValue("string", "app_name", appNameOverride ?: "MuSicX")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
