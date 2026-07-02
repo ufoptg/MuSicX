b2abc026dc4e70425821af4ff03705f8f52e7604
TrueSaiyan <perthgroups@protonmail.com>
2026-07-03 07:45:16 +0800

    Update version to 13.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 152
-        versionName = "13.6.5"
+        versionCode = 153
+        versionName = "13.6.6"
         resValue("string", "app_name", appNameOverride ?: "MuSicX")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
