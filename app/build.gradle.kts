b54b25a1237f580710273b53aa5e8f524e2d7f51
TrueSaiyan <perthgroups@protonmail.com>
2026-07-02 16:17:20 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 149
-        versionName = "13.6.0"
+        versionName = "13.6.1"
         resValue("string", "app_name", appNameOverride ?: "MuSicX")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
